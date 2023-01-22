5a04968ac738bc06b0e436942dab2657acd64bd0
juyonLee00 <ljy012900@gmail.com>
2023-01-22 18:55:01 +0900

    #15 주기를 갖고 움직이는 장애물 추가

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
 
+    //movementFactor : 움직였다가 제자리로 돌아가게 하기 위해 0,1로 범위 설정
+
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    [SerializeField] float movementFactor;
+    [SerializeField] float period = 2f;
 
-    // Start is called before the first frame update
     void Start()
     {
         startingPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //시간에 따라 계속해서 증가
+        float cycles = Time.time / period;
+
+        //상수
+        const float tau = Mathf.PI * 2;
+
+        //-1,1
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        //0,1
+        movementFactor = (rawSinWave + 1f) / 2;
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset; 
     }
